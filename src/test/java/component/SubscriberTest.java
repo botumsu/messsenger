c455b26c708cbafaaab31b2cbe39dc8716508953
Suleyman Karatas <karatasuleyman@gmail.com>
2020-04-07 16:14:11 +0300

    executor is added in unit test

--- src/test/java/component/SubscriberTest.java
+++ src/test/java/component/SubscriberTest.java
@@ -8,7 +8,6 @@ import org.testng.annotations.Test;
 import util.Event;
 import util.EventUpdater;
 
-import java.util.concurrent.Executor;
 import java.util.concurrent.ExecutorService;
 
 import static org.mockito.Mockito.*;
@@ -18,12 +17,14 @@ public class SubscriberTest {
     Subscriber testSubscriber;
     EventChannel eventChannel;
     EventUpdater eventUpdater;
-    Executor executor;
+    ExecutorService executorService;
 
     @BeforeMethod
     public void setUp() {
         eventChannel = mock(EventChannel.class);
         eventUpdater = mock(EventUpdater.class);
+        executorService = mock(ExecutorService.class);
+        implementAsExecutor(executorService);
         testSubscriber = new Subscriber(eventUpdater, eventChannel);
     }
 
@@ -53,13 +54,13 @@ public class SubscriberTest {
     public void testReceiveEvent() {
         //Arrange
         Event event = mock(Event.class);
-        ExecutorService executorService = mock(ExecutorService.class);
         when(eventChannel.getExecutorService()).thenReturn(executorService);
 
         //Act
         testSubscriber.receiveEvent(event);
 
         //verify()
+        verify(eventUpdater,times(1)).onEvent(event);
     }
 
     @Test
@@ -73,5 +74,12 @@ public class SubscriberTest {
 
         //Assert
         Assert.assertEquals(actual, eventUpdater);
+    }
+
+    protected void implementAsExecutor(ExecutorService executor) {
+        doAnswer(invocation -> {
+            ((Runnable) invocation.getArguments()[0]).run();
+            return null;
+        }).when(executor).execute(any(Runnable.class));
     }
 }
